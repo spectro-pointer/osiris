8875cc06646936c60cf6116391c9e091002d900c
Gustavo <gustavosantamaria@hotmail.com>
2023-07-11 14:31:07 +0200

    marcacion de grados

--- init.js
+++ init.js
@@ -2,56 +2,56 @@ const baseURL = "http://192.168.1.30/"
 
 const cameras = [{
   label: "camera1",
-  description: "Camera 1 description",
+  description: "vision 0 a 45 ",
   source: "http://192.168.1.210:8088/video_feed",
   azimuth: { inMin: 0.001, inMax: 1, outMin: 0, outMax: 45 },
   elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax:-25 }
 },
 {
   label: "camera2",
-  description: "Camera 2 description",
+  description: "vision 45 a 90 degres",
   source: "http://192.168.1.210:8088/video_feed",
   azimuth: { inMin: 0.001, inMax: 1, outMin: 45, outMax: 90 },
   elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 }
 },
 {
   label: "camera3",
-  description: "Camera 2 description",
+  description: "vision 90 a 135 degres",
   source: "http://192.168.1.210:8088/video_feed",
   azimuth: { inMin: 0.001, inMax: 1, outMin: 90, outMax: 135 },
   elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 }
 },
 {
   label: "camera4",
-  description: "Camera 2 description",
+  description: "vision 135 a 180 degres",
   source: "http://192.168.1.210:8088/video_feed",
   azimuth: { inMin: 0.001, inMax: 1, outMin: 135, outMax: 180 },
   elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 }
 },
 {
   label: "camera5",
-  description: "Camera 2 description",
+  description: "vision 180 a 225 degres",
   source: "http://192.168.1.210:8088/video_feed",
   azimuth: { inMin: 0.001, inMax: 1, outMin: 180, outMax: 225 },
   elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 }
 },
 {
   label: "camera6",
-  description: "Camera 2 description",
+  description: "vision 225 a 270 Degres",
   source: "http://192.168.1.210:8088/video_feed",
   azimuth: { inMin: 0.001, inMax: 1, outMin: 225, outMax: 270 },
   elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 }
 },
 {
   label: "camera7",
-  description: "Camera 2 description",
+  description: "vision 270 a 315 Degres",
   source: "http://192.168.1.210:8088/video_feed",
   azimuth: { inMin: 0.001, inMax: 1, outMin: 270, outMax: 315 },
   elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 }
 },
 {
   label: "camera8",
-  description: "Camera 2 description",
+  description: "vision 315 a 360 Degres",
   source: "http://192.168.1.210:8088/video_feed",
   azimuth: { inMin: 0.001, inMax: 1, outMin: 315, outMax: 360 },
   elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 }
