36ae11a0be217e1d5edc17d39672c527b9174218
Luc Patiny <luc@patiny.com>
2023-10-31 18:41:49 +0100

    feat: example of code with mouse down

--- buscador.js
+++ buscador.js
@@ -1,32 +1,30 @@
+const baseURL = "http://192.168.1.30/";
 
-const baseURL = "http://192.168.1.30/"
-
-const cameras = [{
-  label: "camera1",
-  description: "Zonn SONY",
-  source: "http://192.168.1.57:8086/video_feed",
-  azimuth: { inMin: 0.001, inMax: 1, outMin: 0, outMax: 360 },
-  elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax:-25 }
-},
-{
-  label: "camera2",
-  description: "py_angular",
-  source: "http://192.168.1.210:8088/video_feed",
-  azimuth: { inMin: 0.001, inMax: 1, outMin: 45, outMax: 90 },
-  elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 }
-},
-{
-  label: "camera3",
-  description: "py_telex",
-  source: "http://192.168.1.210:8088/video_feed",
-  azimuth: { inMin: 0.001, inMax: 1, outMin: 90, outMax: 135 },
-  elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 }
-
-
-}];
+const cameras = [
+  {
+    label: "camera1",
+    description: "Zonn SONY",
+    source: "http://192.168.1.57:8086/video_feed",
+    azimuth: { inMin: 0.001, inMax: 1, outMin: 0, outMax: 360 },
+    elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 },
+  },
+  {
+    label: "camera2",
+    description: "py_angular",
+    source: "http://192.168.1.210:8088/video_feed",
+    azimuth: { inMin: 0.001, inMax: 1, outMin: 45, outMax: 90 },
+    elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 },
+  },
+  {
+    label: "camera3",
+    description: "py_telex",
+    source: "http://192.168.1.210:8088/video_feed",
+    azimuth: { inMin: 0.001, inMax: 1, outMin: 90, outMax: 135 },
+    elevation: { inMin: 0.001, inMax: 1, outMin: 45, outMax: -25 },
+  },
+];
 
 function initCameras() {
-
   const target = document.getElementById("buscador");
 
   for (const camera of cameras) {
@@ -35,7 +33,7 @@ function initCameras() {
     topElement.innerHTML = `
     <h2>${camera.label}</h2>
     <p>${camera.description}</p>
-    `
+    `;
     const cameraElement = document.createElement("div");
     topElement.appendChild(cameraElement);
     cameraElement.className = "video-container";
@@ -48,25 +46,22 @@ function initCameras() {
 
     const videoPlayer = document.getElementById(camera.label);
     addListener(videoPlayer);
-
-
-
   }
 }
 
 initCameras();
 
-const buttons = document.querySelector('.control-buttons');
+const buttons = document.querySelector(".control-buttons");
 
-buttons.addEventListener('mousedown', function(event) {
+buttons.addEventListener("mousedown", function (event) {
   // El cursor del ratón en el momento del evento de ratón
   let shiftX = event.clientX - buttons.getBoundingClientRect().left;
   let shiftY = event.clientY - buttons.getBoundingClientRect().top;
 
   // Centra el buttons bajo el cursor
   function moveAt(pageX, pageY) {
-    buttons.style.left = pageX - shiftX + 'px';
-    buttons.style.top = pageY - shiftY + 'px';
+    buttons.style.left = pageX - shiftX + "px";
+    buttons.style.top = pageY - shiftY + "px";
   }
 
   // Mueve el buttons al momento del click del ratón
@@ -77,18 +72,17 @@ buttons.addEventListener('mousedown', function(event) {
   }
 
   // Mueve el buttons al mover el ratón
-  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener("mousemove", onMouseMove);
 
   // Suelta el buttons al soltar el botón del ratón
-  buttons.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
+  buttons.onmouseup = function () {
+    document.removeEventListener("mousemove", onMouseMove);
     buttons.onmouseup = null;
   };
-
 });
 
 // Previene el comportamiento por defecto del navegador
-buttons.ondragstart = function() {
+buttons.ondragstart = function () {
   return false;
 };
 
@@ -96,20 +90,49 @@ buttons.ondragstart = function() {
 let lastAzimuth = 0;
 let lastElevation = 0;
 
+let intervalEventID;
+const interval = 200; // Intervalo de tiempo en milisegundos
+
 // Define los incrementos
 const azimutIncremento = 1; // Ajusta este valor según sea necesario
 const elevacionIncremento = 1; // Ajusta este valor según sea necesario
 
 // Cuando se haga clic en las flechas, modifica los últimos valores de azimut y elevación y envía una nueva petición.
-document.getElementById('boton-azimut-incrementar').addEventListener('click', () => enviarNuevoValor(lastAzimuth + azimutIncremento, lastElevation));
-document.getElementById('boton-azimut-decrementar').addEventListener('click', () => enviarNuevoValor(lastAzimuth - azimutIncremento, lastElevation));
-document.getElementById('boton-elevacion-incrementar').addEventListener('click', () => enviarNuevoValor(lastAzimuth, lastElevation + elevacionIncremento));
-document.getElementById('boton-elevacion-decrementar').addEventListener('click', () => enviarNuevoValor(lastAzimuth, lastElevation - elevacionIncremento));
+document
+  .getElementById("boton-azimut-incrementar")
+  .addEventListener("mousedown", () => {
+    intervalEventID = setInterval(() => {
+      enviarNuevoValor(lastAzimuth + azimutIncremento, lastElevation);
+    }, interval);
+  });
+document
+  .getElementById("boton-azimut-incrementar")
+  .addEventListener("mouseup", () => {
+    clearInterval(intervalEventID);
+  });
+
+document
+  .getElementById("boton-azimut-decrementar")
+  .addEventListener("click", () =>
+    enviarNuevoValor(lastAzimuth - azimutIncremento, lastElevation)
+  );
+
+document
+  .getElementById("boton-elevacion-incrementar")
+  .addEventListener("click", () =>
+    enviarNuevoValor(lastAzimuth, lastElevation + elevacionIncremento)
+  );
+
+document
+  .getElementById("boton-elevacion-decrementar")
+  .addEventListener("click", () =>
+    enviarNuevoValor(lastAzimuth, lastElevation - elevacionIncremento)
+  );
 // function.js or init.js
 // modo nocturno
-document.getElementById('night-mode-toggle').onclick = function() {
-  document.body.classList.toggle('night-mode');
-}
+document.getElementById("night-mode-toggle").onclick = function () {
+  document.body.classList.toggle("night-mode");
+};
 
 // Historial de valores
 let azimuthHistory = [];
@@ -123,16 +146,24 @@ async function enviarNuevoValor(azimut, elevacion) {
   azimuthHistory.push(lastAzimuth);
   elevationHistory.push(lastElevation);
 
-  // limitar los array 
+  // limitar los array
   if (azimuthHistory.length > 5) {
     azimuthHistory = azimuthHistory.slice(-5);
     elevationHistory = elevationHistory.slice(-5);
   }
 
-  document.getElementById('azimuth-display').textContent = `Azimuth: ${lastAzimuth}`;
-  document.getElementById('azimuth-history').textContent = `Historial de Azimuth: ${azimuthHistory.join(', ')}`;
-  document.getElementById('elevation-display').textContent = `Elevación: ${lastElevation}`;
-  document.getElementById('elevation-history').textContent = `Historial de Elevación: ${elevationHistory.join(', ')}`;
+  document.getElementById(
+    "azimuth-display"
+  ).textContent = `Azimuth: ${lastAzimuth}`;
+  document.getElementById(
+    "azimuth-history"
+  ).textContent = `Historial de Azimuth: ${azimuthHistory.join(", ")}`;
+  document.getElementById(
+    "elevation-display"
+  ).textContent = `Elevación: ${lastElevation}`;
+  document.getElementById(
+    "elevation-history"
+  ).textContent = `Historial de Elevación: ${elevationHistory.join(", ")}`;
 
   const url = baseURL + lastAzimuth + "," + lastElevation;
   const response = await fetch(url);
@@ -161,26 +192,8 @@ function mapRange(value, options) {
 
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // control joisty
- /*
+/*
 var control = document.getElementById("joystick-control");
 var base = document.getElementById("joystick-base");
 
@@ -250,5 +263,3 @@ window.addEventListener("mousemove", function(event) {
 });
 // ...
 */
-
-
